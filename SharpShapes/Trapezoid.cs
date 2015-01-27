6bd3bb62c62f6f7c551248a83b365227cdc34501
JaclynPasquale <jaclynpasquale@gmail.com>
2015-01-27 12:28:06 -0600

    Trapezoid Tests: 9 Failing

--- SharpShapes/Trapezoid.cs
+++ SharpShapes/Trapezoid.cs
@@ -7,6 +7,28 @@ namespace SharpShapes
 {
     public class Trapezoid : Shape
     {
+        private decimal base1;
+        public decimal Base1
+        {
+            get { return this.base1; }
+        }
+        private decimal base2;
+        public decimal Base2
+        {
+            get { return this.base2; }
+        }
+        private decimal altitude;
+        public decimal Altitude
+        {
+            get { return this.altitude;  }
+        }
+        public override int SidesCount
+        {
+            get { return 4; }
+
+        }
+
+
         public Trapezoid(int base1, int base2, int altitude)
         {
             if (base1 <= 0 || base2 <= 0 || altitude <= 0)
@@ -16,6 +38,19 @@ namespace SharpShapes
             this.base1 = base1;
             this.base2 = base2;
             this.altitude = altitude;
+        }
+
+        public override decimal Area()
+        {
+            throw new NotImplementedException();
+        }
+        public override decimal Perimeter()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Scale(int percent)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- TestSharpShapes/UnitTestTrapezoid.cs
+++ TestSharpShapes/UnitTestTrapezoid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpShapes;
 
 namespace TestSharpShapes
 {
@@ -9,7 +10,7 @@ namespace TestSharpShapes
         [TestMethod]
         public void TestTrapezoidConstructor()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             Assert.AreEqual(5, trap.Base1);
             Assert.AreEqual(8, trap.Base2);
             Assert.AreEqual(2, trap.Altitude);
@@ -19,48 +20,48 @@ namespace TestSharpShapes
         [ExpectedException(typeof(ArgumentException))]
         public void TestSanityCheckB2()
         {
-            Trapezoid trap = new trap(0, 2, 3);
+            Trapezoid trap = new Trapezoid(0, 2, 3);
          
         }
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TestSanityCheckB1()
         {
-            Trapezoid trap = new trap(1, 0, 3);
+            Trapezoid trap = new Trapezoid(1, 0, 3);
          
         }
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TestSanityCheckA()
         {
-            Trapezoid trap = new trap(1, 2, 0);
+            Trapezoid trap = new Trapezoid(1, 2, 0);
          
         }
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TestSanityCheckB2negative()
         {
-            Trapezoid trap = new trap(-1, 2, 3);
+            Trapezoid trap = new Trapezoid(-1, 2, 3);
 
         }
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TestSanityCheckB1negative()
         {
-            Trapezoid trap = new trap(1, -1, 3);
+            Trapezoid trap = new Trapezoid(1, -1, 3);
 
         }
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TestSanityCheckAnegative()
         {
-            trap trap = new trap(1, 2, -1);
+            Trapezoid trap = new Trapezoid(1, 2, -1);
 
         }
         [TestMethod]
         public void TestTrapezoidScale100Percent()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             trap.Scale(100);
             Assert.AreEqual(5, trap.Base1);
             Assert.AreEqual(8, trap.Base2);
@@ -70,7 +71,7 @@ namespace TestSharpShapes
         [TestMethod]
         public void TestTrapezoidScale150Percent()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             trap.Scale(150);
             Assert.AreEqual((decimal) 7.5, trap.Base1);
             Assert.AreEqual(12, trap.Base2);
@@ -80,7 +81,7 @@ namespace TestSharpShapes
         [TestMethod]
         public void TestTrapezoidScale200Percent()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             trap.Scale(200);
             Assert.AreEqual(10, trap.Base1);
             Assert.AreEqual(16, trap.Base2);
@@ -90,7 +91,7 @@ namespace TestSharpShapes
         [TestMethod]
         public void TestTrapezoidScale37Percent()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             trap.Scale(37);
             Assert.AreEqual((decimal)1.85, trap.Base1);
             Assert.AreEqual((decimal)2.96, trap.Base2);
@@ -101,14 +102,14 @@ namespace TestSharpShapes
         [ExpectedException(typeof(ArgumentException))]
         public void TestTrapezoidScale0Percent()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             trap.Scale(0);
             
         }
         [TestMethod]
         public void TestTrapezoidScaleUpthenDown()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             trap.Scale(200);
             trap.Scale(50);
             Assert.AreEqual(5, trap.Base1);
@@ -119,38 +120,38 @@ namespace TestSharpShapes
         [TestMethod]
         public void TestTrapezoidSideCount()
         {
-            Trapezoid trap = new trap(4);
-            Assert.AreEqual(4, trap.SideCount);
+            Trapezoid trap = new Trapezoid(4, 6, 4);
+            Assert.AreEqual(4, trap.SidesCount);
 
         }
         [TestMethod]
         public void TestTrapezoidArea()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             Assert.AreEqual(13, trap.Area());
 
         }
         [TestMethod]
         public void TestTrapezoidAreaBiggerArea()
         {
-            Trapezoid trap = new trap(10, 16, 4);
+            Trapezoid trap = new Trapezoid(10, 16, 4);
             Assert.AreEqual(52, trap.Area());
 
         }
         [TestMethod]
         public void TestTrapezoidPerimeter()
         {
-            Trapezoid trap = new trap(5, 8, 2);
+            Trapezoid trap = new Trapezoid(5, 8, 2);
             Assert.AreEqual(16.61, trap.Area());
 
         }
-        [TestMethod]
-        public void TestTrapezoidPerimeter()
-        {
-            Trapezoid trap = new trap(5, 8, 2);
-            Assert.AreEqual(16.61, trap.Area());
+        //[TestMethod]
+        //public void TestTrapezoidBiggerPerimeter()
+        //{
+        //    Trapezoid trap = new trap(20, 40, 8);
+        //    Assert.AreEqual(16.61, trap.Area());
 
-        }
+        //}
 
 
     }
